061d1288822e19cea7e64668680499b646e11400
정원엽 <nerbs0313@gmail.com>
2024-06-29 04:01:16 +0900

    테마 선택 기능 추가

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function PrettyToast(props) {
   );
 }
 
-function LoadBookMark(props) {
+function ModalComp({ changeTheme, selectedTheme }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -47,7 +47,7 @@ function LoadBookMark(props) {
     <div>
       <img className="headingItemOption2" src={`${process.env.PUBLIC_URL}/images/setting.png`} alt="load" onClick={openModal} />
       {isModalOpen && <div className="backdrop" onClick={closeModal} />}
-      {isModalOpen && <Modal closeModal={closeModal} />}
+      {isModalOpen && <Modal closeModal={closeModal} changeTheme={changeTheme} selectedTheme={selectedTheme} />}
     </div>
   );
 }
@@ -437,44 +437,107 @@ const urlCheck = (location) => {
 function App() {
   console.log("start App");
   const [bookmarkCur, bookmarkAfter] = useState("★");
-  const [bgcolor, Setbgcolor] = useState(true);
+  const [theme, setTheme] = useState('light'); // 기본 값은 light로 설정
+  const [selectedTheme, setSelectedTheme] = useState('light');
+  // 애플리케이션 초기화 시, AsyncStorage에서 테마 값을 불러와 설정
+  useEffect(() => {
+    const fetchTheme = async () => {
+      try {
+        const savedTheme = await AsyncStorage.getItem('@theme');
+        if (savedTheme) {
+          setTheme(savedTheme);
+          setSelectedTheme(savedTheme); // 초기 선택 테마 설정
+        }
+      } catch (error) {
+        console.error('Error fetching theme from AsyncStorage:', error);
+      }
+    };
+
+    fetchTheme();
+  }, []);
+
+  // 테마 변경 함수: 테마 변경 후 AsyncStorage에 저장
+  const changeTheme = async (selectedTheme) => {
+    try {
+      await AsyncStorage.setItem('@theme', selectedTheme);
+      setTheme(selectedTheme);
+      setSelectedTheme(selectedTheme); // 테마 변경 시 선택된 테마 업데이트
+    } catch (error) {
+      console.error('Error saving theme to AsyncStorage:', error);
+    }
+  };
 
   const urlParams = new URLSearchParams(window.location.search);
   var email = urlParams.get('email');
   var login = urlParams.get('login');
+  // 테마에 따라 CSS 변수 설정 함수
+  const applyTheme = () => {
+    let BackgroundColor, HeadColor, wordbackColor, wordColor;
 
-  let BackgroundColor = '#F7E3EE';
-  let HeadColor = '#F298C0';
-  let wordbackColor = '#D2C8E3';
-  let wordColor = 'black';
-  if (bgcolor === true) {
-    BackgroundColor = '#F7E3EE';
-    HeadColor = '#F298C0';
-    wordbackColor = `url('https://i.imgur.com/ZRT20vs.png')`;
-    wordColor = 'black';
-  } else {
-    BackgroundColor = 'black';
-    HeadColor = '#1C1C1C';
-    wordbackColor = `url('https://i.imgur.com/KjAaaFN.png')`;
-    wordColor = 'white';
-  }
+    switch (theme) {
+      case 'light':
+        BackgroundColor = '#F7E3EE';
+        HeadColor = '#F298C0';
+        wordbackColor = `url('https://i.imgur.com/ZRT20vs.png')`;
+        wordColor = 'black';
+        break;
+      case 'dark':
+        BackgroundColor = 'black';
+        HeadColor = '#1C1C1C';
+        wordbackColor = `url('https://i.imgur.com/KjAaaFN.png')`;
+        wordColor = 'white';
+        break;
+      case 'peach':
+        BackgroundColor = '#FEEDE3';
+        HeadColor = '#FF9797';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      case 'sky':
+        BackgroundColor = '#E1FAB6';
+        HeadColor = '#D1F3FF';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      case 'autumn':
+        BackgroundColor = '#FEF8B0';
+        HeadColor = '#F6B36E';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      case 'candy':
+        BackgroundColor = '#D3EBF1';
+        HeadColor = '#F6C4D3';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      case 'green':
+        BackgroundColor = '#F6C4D3';
+        HeadColor = '#B8DBD3';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      case 'bug':
+        BackgroundColor = '#FEF8B0';
+        HeadColor = '#F6B36E';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+      default:
+        BackgroundColor = '#EFC6E2';
+        HeadColor = '#DEC4F5';
+        wordbackColor = 'white';
+        wordColor = 'black';
+        break;
+    }
 
-  document.documentElement.style.setProperty(
-    '--background-color', BackgroundColor
-  );
-  document.documentElement.style.setProperty(
-    '--head-background-color', HeadColor
-  );
-  document.documentElement.style.setProperty(
-    '--word-background-color', wordbackColor
-  );
-  document.documentElement.style.setProperty(
-    '--word-color', wordColor
-  );
-  document.documentElement.style.setProperty(
-    '--white-word-BG', `url('https://i.imgur.com/ZRT20vs.png')`
-  );
-
+    document.documentElement.style.setProperty('--background-color', BackgroundColor);
+    document.documentElement.style.setProperty('--head-background-color', HeadColor);
+    document.documentElement.style.setProperty('--word-background-color', wordbackColor);
+    document.documentElement.style.setProperty('--word-color', wordColor);
+  };
+  // 초기 마운트 시 한 번 테마 적용
+  applyTheme();
   return (
     bookmarkCur === '★'
       ? <div>
@@ -485,7 +548,7 @@ function App() {
           <div className="Head_option">
             <SearchInputBox searchInput={() => { }} />
             <RevertColor revertColor={() => {
-              Setbgcolor(!bgcolor);
+              changeTheme(theme === 'light' ? 'dark' : 'light'); // 토글하는 예제로 수정
             }} />
             <OpenBookMark openBookMark={() => {
               bookmarkAfter('☆');
@@ -507,10 +570,7 @@ function App() {
               <LoginInfo email={email} login={login} />
             </div>
             <div className="Head_option2">
-              <PrettyToast />
-              <LoadBookMark loadBookMark={() => {
-                window.location.replace("");
-              }} />
+              <ModalComp changeTheme={changeTheme} selectedTheme={selectedTheme} />
             </div>
           </div>
         </div>
